f8599a60c8c3aa5e97211dbfddce6162f30d6b09
Cheon Park <say8425@gmail.com>
2014-09-15 19:30:50 +0900

    First Init

---
+++ AllTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AllTalk
+//
+//  Created by Cheon Park on 2014. 9. 15..
+//  Copyright (c) 2014년 northPenguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AllTalk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AllTalk
+//
+//  Created by Cheon Park on 2014. 9. 15..
+//  Copyright (c) 2014년 northPenguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
